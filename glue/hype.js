f42198224dc8a2a3626a765849cb909bea5d28cd
Alex Zelenskiy <zelenskiy.alex@gmail.com>
2012-09-29 15:33:42 -0500

    trailing slash

--- glue/hype.js
+++ glue/hype.js
@@ -2,7 +2,7 @@ var request = require('request'),
     host = 'http://singlytics-hyperion.herokuapp.com';
 
 var getApp = function (id, callback) {
-  var url = host + '/analytics/' + id
+  var url = host + '/analytics/' + id + '/'
   console.log('calling hyperion: ' + url)
   request(url, function (error, response, body) {
     if (!error) {
